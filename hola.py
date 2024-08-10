7f3228cc62f93bd9166dfddf919dcc7ecb308696
bedoyaruizmateo2022 <bedoyaruizmateo2022@gmail.com>
2024-08-10 17:42:56 +0000

    pregunto el nombre

--- hola.py
+++ hola.py
@@ -4,3 +4,7 @@ if edad < 18:
     print("no puedes entrar al bar, eres menor de edad")
 else:
     print("puedes entrar al bar, eres mayor de edad")
+
+nombre = input("ingresa tu nombre: ")
+print(f"Tu nombre es {nombre} y tu edad es {edad}")
+
