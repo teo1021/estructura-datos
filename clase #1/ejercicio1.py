38073045ba5e4efda73848e47f98682b8d0e2b9d
bedoyaruizmateo2022 <bedoyaruizmateo2022@gmail.com>
2024-08-17 17:05:50 +0000

    clase 1 ejercicios

--- clase #1/ejercicio1.py
+++ clase #1/ejercicio1.py
@@ -1,29 +1,30 @@
 numero_1 = int(input("ingrese un numero: "))
 numero_2 = int(input("ingrese otro numero: "))
 numero_3 = int(input("ingrese otro numero: "))
+
 if numero_1 > numero_2:
     if numero_1 > numero_3:
-        print(f"el numero mayor es: {numero_1}")
+        print("el numero mayor es: ",numero_1)
     else:
-        print(f"el numero mayor es: {numero_3}")
+        print("el numero mayor es: ",numero_3)
 
 else: 
     if numero_2 > numero_3:
-        print(f"el numero mayor es: {numero_2}")
+        print("el numero mayor es: ", numero_2)
     else:
-        print(f"el numero mayor es: {numero_3}")  
+        print("el numero mayor es: ", numero_3)  
 
 if numero_1 < numero_2:
     if numero_1 < numero_3:
-        print(f"el numero menor es: {numero_1}")
+        print("el numero menor es: ",numero_1)
     else:
-        print(f"el numero menor es: {numero_3}")
+        print("el numero menor es: ",numero_3)
 
 else: 
     if numero_2 < numero_3:
-        print(f"el numero menor es: {numero_2}")
+        print("el numero menor es: ",numero_2)
     else:
-        print(f"el numero menor es: {numero_3}")  
+        print("el numero menor es: ",numero_3)  
               
 
 
